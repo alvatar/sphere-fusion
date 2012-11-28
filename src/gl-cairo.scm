1951fb76f57d944fd8f6b09f90692f64f5f982a8
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-11-28 20:37:27 +0100

    rendering to POT texture for mobile openGL ES

--- src/gl-cairo.scm
+++ src/gl-cairo.scm
@@ -1,97 +1,144 @@
 ;;; Copyright (c) 2012 by Álvaro Castro Castilla
 ;;; Core functionality of Fusion
 
-(define (fusion:critical-error . msgs)
+(define (fusion:error . msgs)
   (SDL_LogError SDL_LOG_CATEGORY_APPLICATION
                 (apply string-append
                        (map (lambda (m) (string-append
                                     (if (string? m) m (object->string m))
                                     " "))
                             msgs)))
+  ;; FIX: this doesn't work for Android
   (exit 1))
 
-(define (fusion:create-cairo-surface width height channels)
-  (let ((buffer (calloc (* channels width height)
-                        sizeof-unsigned-char)))
-    (unless buffer (error "Unable to allocate buffer"))
-    (let ((cairo-surface
-           (cairo_image_surface_create_for_data
-            (void*->unsigned-char* buffer)
-            CAIRO_FORMAT_ARGB32
-            width
-            height
-            (* channels width))))
-      (unless (equal? (cairo_surface_status cairo-surface) CAIRO_STATUS_SUCCESS)
-              (free buffer)
-              (error "Couldn't create cairo surface"))
-      (let ((cairo (cairo_create cairo-surface)))
-        (unless (equal? (cairo_status cairo) CAIRO_STATUS_SUCCESS)
-                (free buffer)
-                (error "Couldn't create context"))
-        (make-will cairo
-                   (lambda (c)
-                     (free buffer)
-                     (cairo_destroy c)))
-        (values cairo
-                buffer)))))
+(define (fusion:error-log . msgs)
+  (SDL_LogError SDL_LOG_CATEGORY_APPLICATION
+                (apply string-append
+                       (map (lambda (m) (string-append
+                                    (if (string? m) m (object->string m))
+                                    " "))
+                            msgs))))
 
-(define (fusion:gl-render-cairo-surface cairo-surface-data texture x0 y0 width height)
-  (unless cairo-surface-data
-          (fusion:critical-error "No valid surface data passed"))
-  (glEnable GL_TEXTURE_2D)
-  (glEnable GL_BLEND)
-  (glBlendFunc GL_SRC_ALPHA GL_ONE_MINUS_SRC_ALPHA)
-  (glPushMatrix)
-  (glBindTexture GL_TEXTURE_2D texture)
-  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR)
-  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
-  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_S GL_CLAMP_TO_EDGE)
-  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_T GL_CLAMP_TO_EDGE)
-  (glMatrixMode GL_MODELVIEW)
-  (glLoadIdentity)
-  (glTexImage2D GL_TEXTURE_2D
-                0
-                (cond-expand (mobile GL_BGRA_EXT) (else GL_RGBA))
-                width
-                height
-                0
-                (cond-expand (mobile GL_BGRA_EXT) (else GL_BGRA))
-                GL_UNSIGNED_BYTE cairo-surface-data)
-  (let ((x0 (exact->inexact x0))
-        (y0 (exact->inexact y0))
-        (x1 (exact->inexact (+ x0 width)))
-        (y1 (exact->inexact (+ y0 height))))
-    (let ((coords
-           (vector->GLfloat* `#(,x0 ,y1
-                                    0.0 0.0
-                                    ,x1 ,y1
-                                    1.0 0.0
-                                    ,x1 ,y0
-                                    1.0 1.0
-                                    ,x0 ,y0
-                                    0.0 1.0)))
-          (indices (vector->GLushort* '#(0 1 2 0 3 2)))
-          (vertex-size (* (+ 2 2) sizeof-GLfloat)))
-      (glEnableClientState GL_VERTEX_ARRAY)
-      (glEnableClientState GL_TEXTURE_COORD_ARRAY)
-      (let* ((vertex-pointer (->void* coords))
-             (texcoords-pointer (void*-offset vertex-pointer
-                                              (* 2 sizeof-GLfloat))))
-        (glVertexPointer 2 GL_FLOAT vertex-size vertex-pointer)
-        (glTexCoordPointer 2 GL_FLOAT vertex-size texcoords-pointer)
-        (glDrawElements GL_TRIANGLES 6 GL_UNSIGNED_SHORT (->void* indices)))))
-  (glPopMatrix)
-  (glDisableClientState GL_TEXTURE_COORD_ARRAY)
-  (glDisableClientState GL_VERTEX_ARRAY)
-  (glDisable GL_BLEND)
-  (glDisable GL_TEXTURE_2D))
+(define (fusion:pot . vals)
+  (let ((largest (apply max vals)))
+    (let recur ((pot 2))
+      (if (< pot largest) (recur (* 2 pot)) pot))))
+
+(define (fusion:create-cairo-surface width height channels)
+  ;; Cairo buffer
+  (let ((cairo-buffer (calloc (* channels width height)
+                              sizeof-unsigned-char)))
+    ;; POT buffer for non-supporting devices
+    (let* ((pot-size (fusion:pot width height))
+           (pot-buffer
+            (cond-expand (mobile (calloc (* channels pot-size pot-size)
+                                           sizeof-unsigned-char))
+                         (else #f))))
+      (unless cairo-buffer (error "Unable to allocate Cairo buffer"))
+      (let ((cairo-surface
+             (cairo_image_surface_create_for_data
+              (void*->unsigned-char* cairo-buffer)
+              CAIRO_FORMAT_ARGB32
+              width
+              height
+              (* channels width))))
+        (unless (equal? (cairo_surface_status cairo-surface) CAIRO_STATUS_SUCCESS)
+                (free cairo-buffer)
+                (free pot-buffer)
+                (fusion:error "Couldn't create cairo surface"))
+        (let ((cairo (cairo_create cairo-surface)))
+          (unless (equal? (cairo_status cairo) CAIRO_STATUS_SUCCESS)
+                  (free cairo-buffer)
+                  (free pot-buffer)
+                  (fusion:error "Couldn't create context"))
+          (make-will cairo
+                     (lambda (c)
+                       (free cairo-buffer)
+                       (free pot-buffer)
+                       (cairo_destroy c)))
+          (values cairo
+                  cairo-buffer
+                  pot-buffer))))))
+
+(define (fusion:create-gl-cairo-surface-renderer texture x0 y0 width height channels)
+  (let* ((pot-size (fusion:pot width height))
+         (update-pot-texture
+          (cond-expand (mobile
+                        (lambda (cairo-surface-data pot-surface-data)
+                          (cairo-copy-surface-to-pot-buffer cairo-surface-data width height
+                                                            pot-surface-data pot-size channels)))
+                       (else
+                        (lambda (c p) (fusion:error "Calling update-pot-texture in a npot-supporting system"))))))
+    (lambda (cairo-surface-data pot-surface-data)
+      (unless cairo-surface-data
+              (fusion:error "No valid surface data passed"))
+      (glEnable GL_TEXTURE_2D)
+      (glEnable GL_BLEND)
+      (glBlendFunc GL_SRC_ALPHA GL_ONE_MINUS_SRC_ALPHA)
+      (glPushMatrix)
+      (glBindTexture GL_TEXTURE_2D texture)
+      (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_NEAREST)
+      (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_NEAREST)
+      (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_S GL_CLAMP_TO_EDGE)
+      (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_T GL_CLAMP_TO_EDGE)
+      (glMatrixMode GL_MODELVIEW)
+      (glLoadIdentity)
+      (cond-expand
+       ;; NPOT not supported in GLES 1.0/1.1
+       (mobile
+        (update-pot-texture cairo-surface-data pot-surface-data)
+        (glTexImage2D GL_TEXTURE_2D 0 GL_BGRA_EXT pot-size pot-size 0 GL_BGRA_EXT GL_UNSIGNED_BYTE pot-surface-data))
+       ;; NPOT supported in OpenGL
+       (else
+        (glTexImage2D GL_TEXTURE_2D 0 GL_RGBA width height 0 GL_BGRA GL_UNSIGNED_BYTE cairo-surface-data)))
+      (let ((x0 (exact->inexact x0))
+            (y0 (exact->inexact y0))
+            (x1 (exact->inexact (+ x0 width)))
+            (y1 (exact->inexact (+ y0 height))))
+        (let ((coords
+               (cond-expand
+                (mobile
+                 (let ((x-tex (exact->inexact (/ width pot-size)))
+                       (y-tex (exact->inexact (/ height pot-size))))
+                   (vector->GLfloat* `#(,x0 ,y1 ;; Rect coordinates
+                                            0.0 0.0 ;; Tex coordinates
+                                            ,x1 ,y1
+                                            ,x-tex 0.0
+                                            ,x1 ,y0
+                                            ,x-tex ,y-tex
+                                            ,x0 ,y0
+                                            0.0 ,y-tex))))
+                (else
+                 (vector->GLfloat* `#(,x0 ,y1     ;; Rect coordinates
+                                          0.0 0.0 ;; Tex coordinates
+                                          ,x1 ,y1
+                                          1.0 0.0
+                                          ,x1 ,y0
+                                          1.0 1.0
+                                          ,x0 ,y0
+                                          0.0 1.0)))))
+              (indices (vector->GLushort* '#(0 1 2 0 3 2)))
+              (vertex-size (* (+ 2 2) sizeof-GLfloat)))
+          (glEnableClientState GL_VERTEX_ARRAY)
+          (glEnableClientState GL_TEXTURE_COORD_ARRAY)
+          (let* ((vertex-pointer (->void* coords))
+                 (texcoords-pointer (void*-offset vertex-pointer
+                                                  (* 2 sizeof-GLfloat))))
+            (glVertexPointer 2 GL_FLOAT vertex-size vertex-pointer)
+            (glTexCoordPointer 2 GL_FLOAT vertex-size texcoords-pointer)
+            (glDrawElements GL_TRIANGLES 6 GL_UNSIGNED_SHORT (->void* indices)))))
+      (glPopMatrix)
+      (glDisableClientState GL_TEXTURE_COORD_ARRAY)
+      (glDisableClientState GL_VERTEX_ARRAY)
+      (glDisable GL_BLEND)
+      (glDisable GL_TEXTURE_2D))))
 
 (define (fusion:create-simple-gl-cairo config)
   (lambda (draw handle-event)
     (let ((init-screen-width (cadr (memq 'width: config)))
           (init-screen-height (cadr (memq 'height: config))))
       (if (< (SDL_Init SDL_INIT_VIDEO) 0)
-          (fusion:critical-error "Couldn't initialize SDL!"))
+          (fusion:error "Couldn't initialize SDL!"))
       ;; SDL
       (let ((win (SDL_CreateWindow
                   ""
@@ -106,15 +153,23 @@
             (screen-width* (make-int* 1))
             (screen-height* (make-int* 1)))
         (SDL_GetWindowSize win screen-width* screen-height*)
-        (let ((check (if (not win) (fusion:critical-error "Unable to create render window" (SDL_GetError))))
+        (let ((check (if (not win) (fusion:error "Unable to create render window" (SDL_GetError))))
               (ctx (SDL_GL_CreateContext win))
               (screen-width (pointer->int screen-width*))
-              (screen-height (pointer->int screen-height*)))
+              (screen-height (pointer->int screen-height*))
+              (texture-channels 4))
           (SDL_Log (object->string screen-width))
           (SDL_Log (object->string screen-height))
           ;; OpenGL
           (let* ((num-textures 1)
-                 (textures (make-GLuint* 1)))
+                 (textures (make-GLuint* 1))
+                 (render-surface (fusion:create-gl-cairo-surface-renderer
+                                  (GLuint*-ref textures 0)
+                                  0
+                                  0
+                                  screen-width
+                                  screen-height
+                                  texture-channels)))
             (SDL_Log (string-append "OpenGL Version: "(glGetString GL_VERSION)))
             ;; OpenGL viewport
             (glViewport 0 0 screen-width screen-height)
@@ -134,8 +189,8 @@
             (glGenTextures num-textures textures)
             ;; Cairo
             (receive
-             (cairo cairo-surface-data)
-             (fusion:create-cairo-surface screen-width screen-height 4)
+             (cairo cairo-surface-data pot-surface-data)
+             (fusion:create-cairo-surface screen-width screen-height texture-channels)
              (let* ((event (make-SDL_Event))
                     (event* (SDL_Event-pointer event)))
                (call/cc
@@ -150,7 +205,7 @@
                     (glClearColor 1.0 0.0 0.0 1.0)
                     (glClear GL_COLOR_BUFFER_BIT)
                     (draw cairo)
-                    (fusion:gl-render-cairo-surface cairo-surface-data (GLuint*-ref textures 0) 0 0 screen-width screen-height)
+                    (render-surface cairo-surface-data pot-surface-data)
                     (SDL_GL_SwapWindow win)
                     (main-loop))))
                (SDL_LogInfo SDL_LOG_CATEGORY_APPLICATION "Bye.")

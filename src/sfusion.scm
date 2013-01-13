755097a176cb12f0d83eacddaf5926c643335686
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-01-13 23:23:19 +0100

    first part of replicating template

--- src/sfusion.scm
+++ src/sfusion.scm
@@ -135,7 +135,17 @@
 
 
 (define (display-help)
-  (pp "HELP!"))
+  (display
+   "
+Usage: sfusion [command] [flags] [operand]
+
+Commands:
+    new (create new project)
+        --template '-t' template to use for project creation
+        [operand] name of the project to create
+    templates (list available templates)
+
+"))
 
 (define sakefile
   '(define sakefile-test 0))
@@ -183,47 +193,65 @@
              output)))
         (display result))))
 
+(define templates-directory "~~spheres/fusion/templates/")
+
+(define (replicate-project-structure project-name project-template)
+  (pp project-name)
+  (pp project-template)
+  (let ((template-dir (string-append templates-directory project-template "/")))
+    (if (file-exists? template-dir)
+        (let recur ((dir template-dir))
+          (for-each (lambda (f)
+                      (let ((path (string-append dir f)))
+                        (if (eq? 'directory (file-type path))
+                            (recur (string-append path "/"))
+                            (println (string-append "DO TEMPLATE FILE: " path)))))
+                    (directory-files dir)))))
+  ;; (if (file-exists? project-name)
+  ;;                   (exit error:file-exists)
+  ;;                   (create-directory project-name))
+  ;; (create-directory (string-append project-name "/doc"))
+  ;; (create-directory (string-append project-name "/src"))
+  ;; (create-directory (string-append project-name "/test"))
+  ;; (call-with-output-file
+  ;;     (string-append project-name "/sakefile.scm")
+  ;;   (lambda (f) (pp sakefile f)))
+  ;; (call-with-output-file
+  ;;                   (string-append project-name "/README.md")
+  ;;                 (lambda (f) (display "" f)))
+  ;; (process-template "text-template" (string-append project-name "/config.scm"))
+  )
+
  (define (main)
-  (cond
-   ((null? (cdr (command-line)))
-    (display-help)
-    (exit))
-   ((string=? (cadr (command-line)) "help")
-    (display-help))
-   ((string=? (cadr (command-line)) "new")
-    (receive (project-name templates)
-             (args-fold (cddr (command-line))
-                        ;; Option processors
-                        (list (option '(#\t "templates") #t #f
-                                      (lambda (option name arg templates)
-                                        (values arg))))
-                        ;; Unrecognized option processor
-                        (lambda (option name arg . seeds)
-                          (println (string-append "Unrecognized option: " name))
-                          (exit error:invalid-argument))
-                        ;; Operand processor: its output gets passed to receive
-                        (lambda (operand templates)
-                          (values operand templates))
-                        ;; Default argument values
-                        '())
-             ;; Directory structure creation
-             (if (file-exists? project-name)
-                 (exit error:file-exists)
-                 (create-directory project-name))
-             (create-directory (string-append project-name "/doc"))
-             (create-directory (string-append project-name "/src"))
-             (create-directory (string-append project-name "/test"))
-             (call-with-output-file
-                 (string-append project-name "/sakefile.scm")
-               (lambda (f) (pp sakefile f)))
-             (process-template "text-template" (string-append project-name "/config.scm"))
-             (call-with-output-file
-                 (string-append project-name "/README.md")
-               (lambda (f) (display "" f)))
-             (pp project-name)
-             (pp templates)))
-   (else
-    (println "Sfusion: unrecognized command"))))
+   (cond
+    ((null? (cdr (command-line)))
+     (display-help)
+     (exit))
+    ((string=? (cadr (command-line)) "help")
+     (display-help))
+    ((string=? (cadr (command-line)) "new")
+     (receive (project-name template)
+              (args-fold (cddr (command-line))
+                         ;; Option processors
+                         (list (option '(#\t "template") #t #f
+                                       (lambda (option name arg template)
+                                         (values arg))))
+                         ;; Unrecognized option processor
+                         (lambda (option name arg . seeds)
+                           (println (string-append "Unrecognized option: " name))
+                           (exit error:invalid-argument))
+                         ;; Operand processor: its output gets passed to receive
+                         (lambda (operand template)
+                           (values operand template))
+                         ;; Default argument values
+                         '())                                
+              (replicate-project-structure project-name template)))
+    ((string=? (cadr (command-line)) "template")
+     (println "Available templates:")
+     (for-each (lambda (d) (display "  - ") (println d))
+               (directory-files templates-directory)))
+    (else
+     (println "Sfusion: unrecognized command"))))
 
 (main)
 

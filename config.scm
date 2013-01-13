9f9ac535c4de99c430e6391ff37fa596eece7457
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-01-13 21:07:04 +0100

    sfusion infraestructure

--- config.scm
+++ config.scm
@@ -1,22 +1,12 @@
 (sphere: "fusion")
 (dependencies:
- (driver
+ (sfusion
   (load
-   (sdl2: sdl2)))
- ((= driver version: (debug))
-  (load
-   (sdl2: sdl2 version: (debug))))
+   (energy: program-arguments)))
  (gl-cairo
   (load
    (sdl2: sdl2)
    (cairo: cairo)
    (cond-expand (mobile (opengl: gl-es)
                         (opengl: gl-es-ext))
-                (else (opengl: gl)))))
- ((= gl-cairo version: (debug))
-  (load
-   (sdl2: sdl2 version: (debug))
-   (cairo: cairo version: (debug))
-   (cond-expand (mobile (opengl: gl-es version: (debug))
-                        (opengl: gl-es-ext version: (debug)))
-                (else (opengl: gl version: (debug)))))))
+                (else (opengl: gl))))))

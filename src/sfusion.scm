d616d3291cec3cb036b79031e8b9c24e74db39eb
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-01-17 20:59:02 +0100

    first version of template system

--- src/sfusion.scm
+++ src/sfusion.scm
@@ -153,66 +153,165 @@ Commands:
 (define sakefile
   '(define sakefile-test 0))
 
-;; An idea of how templates could be:
-;; <title>=(string-append "Hello" " world")</title>
-;; <h1>Header</h1>
-;; <h2>Header 2 will receive an injected variable from the caller: =(let-templates () (external-var) (object->string external-var))</h2>
-;; =(let-templates (a b) () ; this template won't use the external-var so it doesn't need to specify it
-;;    (form-for shoe html: '((multipart #t))
-;;      (lambda (f)
-;;        (if (not (null? shoes))
-;;            (a f shoes) ; template a is passed the variables "f" and "shoes", template b doesn't get any variable
-;;            (b)))))
-;; -( <div>Text 1</div>
-;;    =(let-templates (aa) (f shoes) ; we need to receive the parameters passed by the parent template. If empty list then the params are not passed
-;;       (string-append "<div>" (aa) "</div>"))
-;;    -( just some plain text inside text1, which will be surrounded by divs )
-;;    <div>Text 1 (continues)</div> )
-;; -( <div>Text 2</div> )
-
-
-(define (process-template template-file output-file)
-  (define (evaluate-template-code char-list)
-    (let recur ((cs char-list))
-      (cond ((null? cs)
-             '())
-            ;; open code portion
-            ((and (char=? #\# (car cs))
-                  (not (null? (cddr cs)))
-                  (char=? #\| (cadr cs)))
-             (receive (code-char-list rest)
-                      (let code-loop ((code-cs (cddr cs)))
-                        (cond ((null? code-cs)
-                               (error "malformed template"))
-                              ;; close code portion
-                              ((and (char=? #\| (car code-cs))
-                                    (not (null? (cddr code-cs)))
-                                    (char=? #\# (cadr code-cs)))
-                               (values '() (cddr code-cs)))
-                              (else (receive (code-char-list rest)
-                                             (code-loop (cdr code-cs))
-                                             (values (cons (car code-cs)
-                                                           code-char-list)
-                                                     rest)))))
-                      (recur (append
-                              (string->list
-                               (eval (cons 'begin (with-input-from-string (list->string code-char-list) read-all))))
-                              rest))))
-            (else (cons (car cs)
-                        (recur (cdr cs)))))))
-  (let ((result (list->string
-                 (evaluate-template-code
-                  (call-with-input-file
-                      template-file
-                    (lambda (i) (read-all i read-char)))))))
-    (if output-file
-        (call-with-output-file
-            output-file
-          (lambda (output)
-            (display
-             result
-             output)))
-        (display result))))
+;; Arguments are passed to sub-templates in the variable "args"
+;; ((eval
+;;   (process-template "
+;; <title>=(let-template (a b) (string-append \"Hello\" \" world.\n\" (a) (b 8585858)))=
+;; -(try )-
+;; -(this =((car args))= times!)-
+;; </title>
+;; <h1>Header =(input-var)=</h1>
+;; " '(input-var)))
+;;  "passed variable!")
+(define (process-template template input-variables)
+  (letrec
+      ((template-code-begin?
+        (lambda (strl) (and (not (null? strl))
+                       (char=? #\= (car strl))
+                       (not (null? (cdr strl)))
+                       (char=? #\( (cadr strl)))))
+       (template-code-end?
+        (lambda (strl) (and (not (null? strl))
+                       (char=? #\) (car strl))
+                       (not (null? (cdr strl)))
+                       (char=? #\= (cadr strl)))))
+       (template-argstring-begin?
+        (lambda (strl last-token-type)
+          (if (or (eq? last-token-type 'code)
+                  (eq? last-token-type 'arg-string))
+              (and (not (null? strl))
+                   (char=? #\( (car strl)))
+              (and (not (null? strl))
+                   (char=? #\- (car strl))
+                   (not (null? (cdr strl)))
+                   (char=? #\( (cadr strl))))))
+       (template-argstring-end?
+        (lambda (strl) (and (not (null? strl))
+                       (char=? #\) (car strl))
+                       (not (null? (cdr strl)))
+                       (char=? #\- (cadr strl)))))
+       (generate-code
+        (lambda (tokens)
+          (letrec
+              ((generate-template-level
+                (lambda (tokens)
+                  `(string-append
+                    ,@(let recur ((tok tokens))
+                        (cond ((null? tok) '())
+                              ;; Generate free strings
+                              ((eq? 'free-string (caar tok))
+                               (if (string-null? (cdar tok))
+                                   (recur (cdr tok)) ; Remove if zero-length
+                                   (cons (cdar tok)
+                                         (recur (cdr tok)))))
+                              ;; Generate code part and its arguments
+                              ((eq? 'code (caar tok))
+                               (let-values (((template-processor)
+                                             (with-input-from-string (cdar tok) read))
+                                            ((template-args rest)
+                                             (generate-template-sublevels (cdr tok))))
+                                 (cond ((eq? (car template-processor) 'let-template)
+                                        (if (not (= (length (cadr template-processor))
+                                                    (length template-args)))
+                                            (error "Number of template processor parameters and given sub-templates don't match"))
+                                        (cons `(,(cons 'lambda (cdr template-processor))
+                                                ,@(map (lambda (arg) `(lambda args (string-append ,@(recur arg)))) template-args))
+                                              (recur rest)))
+                                       (else
+                                        (cons `((lambda (o) (if (string? o) o (object->string o))) ,(car template-processor))
+                                              (recur rest))))))
+                              ((eq? 'arg-string (caar tok))
+                               (error "Nested templates must come after a template processor"))
+                              (else
+                               (error "error generating code")))))))
+               (generate-template-sublevels
+                (lambda (tokens)
+                  (let recur ((tok tokens))
+                    (cond ((null? tok) (values '() '()))
+                          ((eq? (caar tok) 'arg-string)
+                           (receive (template-args rest)
+                                    (recur (cdr tok))
+                                    (values (cons (cdar tok)
+                                                  template-args)
+                                            rest)))
+                          (else
+                           (values '()
+                                   tok)))))))
+            (generate-template-level tokens))))
+       (parse
+        (lambda (template-char-list)
+          (receive (tokens rest)
+                   (let token-recur ((strl template-char-list)
+                                     (last-token-type 'none))
+                     (cond
+                      ((null? strl) (values '() '()))
+                      ;; Code tokens
+                      ((template-code-begin? strl)
+                       (receive (chars rest)
+                                (let char-recur ((strl (cdr strl))
+                                                 (inside-code-string? #f))
+                                  (cond ((null? strl) (values '() '()))
+                                        ((and (template-code-end? strl)
+                                              (not inside-code-string?))
+                                         (values '(#\)) (cddr strl)))
+                                        (else (receive (chars rest)
+                                                       (char-recur (cdr strl)
+                                                                   (if inside-code-string?
+                                                                       (not (char=? #\" (car strl)))
+                                                                       (char=? #\" (car strl))))
+                                                       (values (cons (car strl)
+                                                                     chars)
+                                                               rest)))))
+                                (receive (tokens rest)
+                                         (token-recur rest 'code)
+                                         (values (cons (cons 'code (list->string chars))
+                                                       tokens)
+                                                 rest))))
+                      ;; Argument string tokens
+                      ((template-argstring-begin? strl last-token-type)
+                       (receive (chars rest)
+                                (let char-recur ((strl (if (or (eq? last-token-type 'code)
+                                                               (eq? last-token-type 'arg-string))
+                                                           (cdr strl)
+                                                           (cddr strl))))
+                                  (cond ((null? strl) (values '() '()))
+                                        ((template-argstring-end? strl)
+                                         (values '() (cddr strl)))
+                                        (else (receive (chars rest)
+                                                       (char-recur (cdr strl))
+                                                       (values (cons (car strl)
+                                                                     chars)
+                                                               rest)))))
+                                (receive (tokens rest)
+                                         (token-recur rest 'arg-string)
+                                         (values (cons (cons 'arg-string (parse chars))
+                                                       tokens)
+                                                 rest))))
+                      ;; Ignored
+                      ((char=? (car strl) #\newline)
+                       (token-recur (cdr strl)
+                                    'ignored))
+                      ;; Free string tokens
+                      (else
+                       (receive (chars-for-string rest)
+                                (let char-recur ((strl strl))
+                                  (cond ((null? strl) (values '() '()))
+                                        ((template-code-begin? strl)
+                                         (values '() strl))
+                                        ((template-argstring-end? strl)
+                                         (values '() (cddr strl)))
+                                        (else (receive (chars-for-string rest)
+                                                       (char-recur (cdr strl))
+                                                       (values (cons (car strl)
+                                                                     chars-for-string)
+                                                               rest)))))
+                                (receive (tokens rest)
+                                         (token-recur rest 'free-string)
+                                         (values (cons (cons 'free-string (list->string chars-for-string))
+                                                       tokens)
+                                                 rest))))))
+                   tokens))))
+    `(lambda ,input-variables ,(generate-code (parse (string->list template))))))
 
 (define templates-directory "~~spheres/fusion/templates/")
 

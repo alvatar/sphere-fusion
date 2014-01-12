56cdc5681898444d5ceefe8fba22bb6acc07e73b
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2014-01-12 20:18:36 +0100

    updated config due to relocated functional module

--- config.scm
+++ config.scm
@@ -5,9 +5,9 @@
    (core: base-macros)
    (energy: functional-arguments-macros))
   (load
+   (core: functional)
    (fabric: algorithm/list)
    (energy: error-code)
-   (energy: functional)
    (energy: functional-arguments)
    (energy: template)))
  (core

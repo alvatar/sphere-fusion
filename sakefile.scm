4e7a93361e70e4fb21bc91d9d171ee5c2a8a17b4
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2014-01-31 16:34:25 +0100

    properly install by default

--- sakefile.scm
+++ sakefile.scm
@@ -8,18 +8,6 @@
   (sake#compile-to-exe "sfusion" '(sfusion) cond-expand-features: '(optimize)))
 
 (define-task post-compile ()
-  ;; Create SDL symlink in SDL-based generators
-  ;; When installed, files get actually copied as directory recursion is done.
-  #;(let ((SDL-link "generators/opengl2d/android/android/jni/SDL"))
-    (if (file-exists? SDL-link)
-        (##delete-file SDL-link))
-    (create-symbolic-link (string-append (%sphere-path 'sdl2) "src/android/jni/SDL")
-                          (string-append (current-directory) SDL-link)))
-  
-  ;; Make Fusion modules available in /lib
-  'post-compile)
-
-(define-task install-binary-and-sake-extensions ()
   ;; Install Sfusion
   (copy-file (string-append (current-build-directory) "sfusion")
              "~~bin/sfusion")
@@ -28,8 +16,8 @@
              "~~spheres/sake-extensions/src/fusion.scm")
   (copy-file (string-append (current-source-directory) "sake-extensions/template.scm")
              "~~spheres/sake-extensions/src/template.scm"))
-
-(define-task install (install-binary-and-sake-extensions)
+ 
+(define-task install ()
   (sake#install-sphere-to-system extra-directories: '("generators")))
 
 (define-task clean ()

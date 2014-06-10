3886795d5a366f05fd07f826156026ccb51b017d
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2014-06-10 23:25:24 +0200

    fixed host-run-interpreted issue with cond-expand

--- src/sake-extensions/fusion.scm
+++ src/sake-extensions/fusion.scm
@@ -603,15 +603,48 @@
 (define (fusion#host-run-interpreted main-module #!key
                                      (version '())
                                      (cond-expand-features '()))
-  (let ((code `(;;(##namespace (,(string-append (symbol->string (gensym 'sakefile)) "#")))
-                ;;(##include "~~lib/gambit#.scm")
-                ;;(##include "~~spheres/core/src/sake/sakelib#.scm")
-                ;;(##namespace ("" alexpand))
-                ;;(##include "~~spheres/spheres#.scm")
-                (define-cond-expand-feature host)
-                ,@(map (lambda (f) `(define-cond-expand-feature ,f)) cond-expand-features)
-                (##spheres-load ,main-module))))
-   (gambit-eval-here code)))
+  (let* ((features (cons 'host cond-expand-features))
+         (code `((define-syntax syntax-rules-error
+                   (syntax-rules ()
+                     ((_) (0))))
+                 (define-syntax cond-expand
+                   (syntax-rules (and or not else ,@features)
+                     ((cond-expand) (syntax-rules-error "Unfulfilled cond-expand"))
+                     ((cond-expand (else body ...))
+                      (begin body ...))
+                     ((cond-expand ((and) body ...) more-clauses ...)
+                      (begin body ...))
+                     ((cond-expand ((and req1 req2 ...) body ...) more-clauses ...)
+                      (cond-expand
+                       (req1
+                        (cond-expand
+                         ((and req2 ...) body ...)
+                         more-clauses ...))
+                       more-clauses ...))
+                     ((cond-expand ((or) body ...) more-clauses ...)
+                      (cond-expand more-clauses ...))
+                     ((cond-expand ((or req1 req2 ...) body ...) more-clauses ...)
+                      (cond-expand
+                       (req1
+                        (begin body ...))
+                       (else
+                        (cond-expand
+                         ((or req2 ...) body ...)
+                         more-clauses ...))))
+                     ((cond-expand ((not req) body ...) more-clauses ...)
+                      (cond-expand
+                       (req
+                        (cond-expand more-clauses ...))
+                       (else body ...)))
+                     ,@(map
+                        (lambda (cef)
+                          `((cond-expand (,cef body ...) more-clauses ...)
+                            (begin body ...)))
+                        features)
+                     ((cond-expand (feature-id body ...) more-clauses ...)
+                      (cond-expand more-clauses ...))))
+                 (##spheres-load ,main-module))))
+    (gambit-eval-here code)))
 
 (define (fusion#host-compile-exe exe-name main-module #!key
                                  (version '())

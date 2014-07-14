1d0d93ed46609b95d437f0fcebe19954c197f273
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2014-07-14 19:17:02 +0200

    build syntax-case for ios and Android

--- sakefile.scm
+++ sakefile.scm
@@ -1,3 +1,7 @@
+(include "src/sake-extensions/fusion-android.scm")
+(include "src/sake-extensions/fusion-ios.scm")
+
+
 (define non-compiled-modules
   '(core))
 
@@ -20,8 +24,11 @@
              "~~spheres/sake-extensions/src/fusion-ios.scm"))
 
 (define-task install-2 ()
+  ;; Delete Android toolchain prior to installing the directory
+  (if (file-exists? "test/tmp")
+      (sake#delete-file (string-append (android-toolchain-directory)) recursive: #t))
   ;; Install the Sphere's modules and extra directories
-  (sake#install-sphere-to-system extra-directories: '("generators" "tools")))
+  (sake#install-sphere-to-system extra-directories: '("generators" "tools" "android" "ios")))
 
 (define-task install (install-1 install-2)
   'install)
@@ -29,11 +36,79 @@
 (define-task clean ()
   (sake#default-clean)
   (if (file-exists? "test/tmp")
-      (sake#delete-file "test/tmp" force: #t recursive: #t)))
+      (sake#delete-file "test/tmp" force: #t recursive: #t))
+  (let ((android-sc (string-append (android-directory) "syntax-case.o1")))
+    (if (file-exists? android-sc)
+        (sake#delete-file android-sc force: #t)))
+  (let ((android-sc (string-append (ios-directory) "syntax-case.o1")))
+    (if (file-exists? android-sc)
+        (sake#delete-file android-sc force: #t))))
 
 (define-task all (compile)
   'all)
 
+;;------------------------------------------------------------------------------
+;; Precompiled Android & iOS modules
+
+(define-task compile-android-syntax-case ()
+  (fusion#android-installed?)
+  ;; Generate Android toolchain
+  (let ((android-platform-str "android-15"))
+    (shell-command
+     (string-append (android-ndk-path) "build/tools/make-standalone-toolchain.sh"
+                    " --platform=\"" android-platform-str "\""
+                    " --install-dir=\"" (android-toolchain-directory) "\"")))
+  ;; Compile syntax-case to C
+  (gambit-eval-here
+   `((parameterize ((current-readtable (readtable-sharing-allowed?-set (current-readtable) 'serialize)))
+                   (compile-file-to-target
+                    ,(string-append (%sphere-path 'core) "src/scsc/syntax-case.scm")
+                    output: ,(string-append (current-build-directory) "syntax-case.o1.c"))))
+   flags-string: "-f")
+  ;; Compile .o1 module
+  (fusion#android-run-compiler arch: 'arm
+                               compiler: 'gcc
+                               arguments:
+                               `(,(string-append "-I" (path-expand "~~/include/"))
+                                 "-shared"
+                                 "-Wno-unused" "-Wno-write-strings"
+                                 "-fno-math-errno" "-fschedule-insns2" "-fno-common"
+                                 "-fno-trapping-math" "-fno-strict-aliasing" "-fwrapv" "-fomit-frame-pointer" "-fPIC"
+                                 ;; "-mieee-fp " "-flat_namespace" "-undefined" "suppress" ;; (not available in ARM gcc)
+                                 "-D___DYNAMIC"
+                                 "-D___SINGLE_HOST" "-O2" ;; optimizations
+                                 "-o" ,(string-append (android-directory) "syntax-case.o1")
+                                 ,(string-append (current-build-directory) "syntax-case.o1.c"))
+                               verbose: #t)
+  ;; Cleanup Android toolchain
+  (sake#delete-file (string-append (android-toolchain-directory)) recursive: #t))
+
+(define-task compile-ios-syntax-case ()
+  ;; Compile syntax-case to C
+  (gambit-eval-here
+   `((parameterize ((current-readtable (readtable-sharing-allowed?-set (current-readtable) 'serialize)))
+                   (compile-file-to-target
+                    ,(string-append (%sphere-path 'core) "src/scsc/syntax-case.scm")
+                    output: ,(string-append (current-build-directory) "syntax-case.o1.c"))))
+   flags-string: "-f")
+  ;; Compile .o1 module for the simulator
+  (fusion#ios-run-compiler arch: 'i386
+                           platform-type: 'simulator
+                           compiler: 'gcc
+                           arguments:
+                           `(,(string-append "-I" (path-expand "~~/include/"))
+                             "-bundle"
+                             "-Wno-unused" "-Wno-write-strings" "-fno-math-errno" "-fno-common"
+                             "-fno-trapping-math" "-fno-strict-aliasing" "-fwrapv" "-fomit-frame-pointer" "-fPIC"
+                             "-flat_namespace" "-undefined" "suppress"
+                             ;; "-mieee-fp" "-fschedule-insns2" ;; (not available in iOS C compiler)
+                             "-D___DYNAMIC"
+                             "-D___SINGLE_HOST" "-O2" ;; optimizations
+                             "-o" ,(string-append (ios-directory) "syntax-case.o1")
+                             ,(string-append (current-build-directory) "syntax-case.o1.c"))
+                           verbose: #t)
+  ;; Cleanup Android toolchain
+  (sake#delete-file (string-append (android-toolchain-directory)) recursive: #t))
 
 ;;------------------------------------------------------------------------------
 ;; Tests
